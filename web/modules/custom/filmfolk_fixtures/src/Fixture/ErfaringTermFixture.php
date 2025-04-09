b6ce531d1c853d16e5788bfc6482c551c32e49b6
Mikkel Ricky <rimi@aarhus.dk>
2025-04-09 13:00:44 +0200

    Added erfaring taxonomy

---
+++ web/modules/custom/filmfolk_fixtures/src/Fixture/ErfaringTermFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\filmfolk_fixtures\Fixture;
+
+/**
+ * Erfaring term fixture.
+ */
+class ErfaringTermFixture extends AbstractTaxonomyTermFixture {
+  /**
+   * {@inheritdoc}
+   */
+  protected static string $vocabularyId = 'erfaring';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static array $terms = [
+    '0 produktioner',
+    '1 produktioner',
+    '2 produktioner',
+    '3 produktioner',
+    '4 produktioner',
+    '5 produktioner',
+    '6 produktioner',
+    '7 produktioner',
+    '8 produktioner',
+    '9 produktioner',
+    '10+ produktioner',
+  ];
+
+}
