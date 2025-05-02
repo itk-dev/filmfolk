d13d6dc39e99ead9e48c2bc0bd9593fbac258989
Jesper Pedersen <pejes@aarhus.dk>
2025-05-02 10:02:43 +0200

    Add some more users to fixtures

--- web/modules/custom/filmfolk_fixtures/src/Fixture/UserFixture.php
+++ web/modules/custom/filmfolk_fixtures/src/Fixture/UserFixture.php
@@ -61,6 +61,114 @@ final class UserFixture extends AbstractFixture implements DependentFixtureInter
     $user->save();
 
     $user = User::create([
+      'mail' => 'user003@example.com',
+      'field_navn' => 'Navn Navnesen',
+      'field_kommune' => $this->getReference('kommune:Herning'),
+      'field_funktion' => [
+        $this->getReference('funktion:Runner'),
+        $this->getReference('funktion:Scenograf'),
+      ],
+      'field_funktion_erfaring' => [
+        [
+          // Important: We must use “…_TARGET_ID” to make this work.
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Runner')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:7 produktioner')->id(),
+        ],
+        [
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Advokat')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:1 produktion')->id(),
+        ],
+      ],
+      'field_description' => [
+        'value' => 'Hvor\' du fra? Jeg\' fra <strong>Baune!</strong>',
+        'format' => 'simple',
+      ],
+    ])
+      ->activate();
+    $user->save();
+
+    $user = User::create([
+      'mail' => 'user004@example.com',
+      'field_navn' => 'Navn Navnesen',
+      'field_kommune' => $this->getReference('kommune:Herning'),
+      'field_funktion' => [
+        $this->getReference('funktion:Runner'),
+        $this->getReference('funktion:Scenograf'),
+      ],
+      'field_funktion_erfaring' => [
+        [
+          // Important: We must use “…_TARGET_ID” to make this work.
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Runner')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:7 produktioner')->id(),
+        ],
+        [
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Advokat')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:1 produktion')->id(),
+        ],
+      ],
+      'field_description' => [
+        'value' => 'Hvor\' du fra? Jeg\' fra <strong>Baune!</strong>',
+        'format' => 'simple',
+      ],
+    ])
+      ->activate();
+    $user->save();
+
+    $user = User::create([
+      'mail' => 'user005@example.com',
+      'field_navn' => 'Navn Navnesen',
+      'field_kommune' => $this->getReference('kommune:Herning'),
+      'field_funktion' => [
+        $this->getReference('funktion:Runner'),
+        $this->getReference('funktion:Scenograf'),
+      ],
+      'field_funktion_erfaring' => [
+        [
+          // Important: We must use “…_TARGET_ID” to make this work.
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Runner')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:7 produktioner')->id(),
+        ],
+        [
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Advokat')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:1 produktion')->id(),
+        ],
+      ],
+      'field_description' => [
+        'value' => 'Hvor\' du fra? Jeg\' fra <strong>Baune!</strong>',
+        'format' => 'simple',
+      ],
+    ])
+      ->activate();
+    $user->save();
+
+    $user = User::create([
+      'mail' => 'user006@example.com',
+      'field_navn' => 'Navn Navnesen',
+      'field_kommune' => $this->getReference('kommune:Herning'),
+      'field_funktion' => [
+        $this->getReference('funktion:Runner'),
+        $this->getReference('funktion:Scenograf'),
+      ],
+      'field_funktion_erfaring' => [
+        [
+          // Important: We must use “…_TARGET_ID” to make this work.
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Runner')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:7 produktioner')->id(),
+        ],
+        [
+          FunktionErfaringItem::PROPERTY_FUNKTION_TARGET_ID => $this->getReference('funktion:Advokat')->id(),
+          FunktionErfaringItem::PROPERTY_ERFARING_TARGET_ID => $this->getReference('erfaring:1 produktion')->id(),
+        ],
+      ],
+      'field_description' => [
+        'value' => 'Hvor\' du fra? Jeg\' fra <strong>Baune!</strong>',
+        'format' => 'simple',
+      ],
+    ])
+      ->activate();
+    $user->save();
+
+    $user = User::create([
       'mail' => 'user1@example.com',
       'field_kommune' => $this->getReference('kommune:Aarhus'),
     ]);
